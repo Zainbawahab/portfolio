aa48d17326530656c140562bcf32111853d2b0ed
Zainbawahab <Zainbawahab@zains-mbp.lan>
2015-05-26 00:48:28 -0400

    Added my content and scroll images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,19 @@
     	e.preventDefault();
     	$(this).add('#slidebar').toggleClass('is-open'); 
   	});
+/*******ImageScroll********/
+   /* $('.img-holder').imageScroll({
+      coverRatio: 0.70
+    });
+    $('.img-holder').imageScroll({
+      touch: true
+    });*/
 
+    /*  var touch = Modernizr.touch;
+    $('.img-holder').imageScroll({
+      imageAttribute: (touch === true) ? 'image-mobile' : 'image',
+    touch: touch
+    }); */
 
   });
 })(jQuery);
